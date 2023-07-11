23509d34ab78bfbb22b9120ec03a9c6b01d14c79
“Andrew <andrew.cheung.2@stonybrook.edu>
2023-07-11 15:40:12 -0400

    changing profile pic and about is fully functional

--- src/client/src/pages/Profile.js
+++ src/client/src/pages/Profile.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { usePostsContext } from '../hooks/usePostsContext'
 import { useAuthContext } from '../hooks/useAuthContext'
-import avatar from '../assets/avatar.png' 
+import avatarIcon from '../assets/avatar.png' 
 
 // components
 import PostDetails from '../components/PostDetails'
@@ -13,15 +13,16 @@ const Profile = () => {
   const { posts, dispatch } = usePostsContext()
   const { user } = useAuthContext()
 
-
-
-  const [ image, setImage] = useState("")
+  const [ avatar, setAvatar] = useState("")
   const [ about, setAbout] = useState("")
+  const [ newAvatar, setNewAvatar ] = useState("")
+  const [ newAbout, setNewAbout ] = useState("")
+  const [ hide, setHide ] = useState(true)
 
   const handleFileUpload = async (e) => {
     const file = e.target.files[0]
     const base64 = await convertToBase64(file)
-    setImage(base64)
+    setNewAvatar(base64)
   }
 
   useEffect(() => {
@@ -49,9 +50,8 @@ const Profile = () => {
       })
 
       const json = await response.json()
-      console.log(json)
 
-      setImage(json.profilePicture)
+      setAvatar(json.profilePicture)
       setAbout(json.about)
       
 
@@ -60,21 +60,17 @@ const Profile = () => {
     if (user) {
       fetchPosts()
       fetchUser()
-      console.log(image)
-      console.log(about)
     }
-
-  
-
-  }, [dispatch, user, username, image, about])
+  }, [dispatch, user, username, avatar, about])
 
   const handlePicUpload = async (e) => {
     e.preventDefault()
 
-    const response = await fetch('/api/user/' + user.username, {
+    const response = await fetch('/api/user/Profile/' + user.username, {
       method: 'PATCH',
       body: JSON.stringify({
-        profilePicture: image
+        about: newAbout || about,
+        profilePicture: newAvatar || avatar
       }),
       headers: {
         'Authorization' : `Bearer ${user.token}`,
@@ -82,45 +78,57 @@ const Profile = () => {
       }
     })
     const json = await response.json()
-    console.log(json)
+    json.about = newAbout
+    json.profilePicture = newAvatar
 
-    const response2 = await fetch('/api/user/' + user.username, {
-      method: 'GET',
-      headers: {
-        'Authorization' : `Bearer ${user.token}`,
-      }
-    })
-    const json2 = await response2.json()
+    setAvatar(newAvatar);
+    setAbout(newAbout)
+    setNewAvatar("")
+    setNewAbout("")
+    setHide(!hide)
+  }
+  
+  const handleHide = async(e) => {
+    e.preventDefault()
 
-    setImage(json2.profilePicture);
-
+    setHide(!hide)
+    console.log(hide)
   }
 
   return ( 
     <div className="home">
       <div className="profile">
         <h1>{username}</h1>
-        <img className="profile-picture" src={image || avatar} alt="" />
+        <img className="profile-picture" src={avatar || avatarIcon} alt="" />
         <br />
         { 
-          username === user.username && (
-          <>
-          <label htmlFor="prof-file-upload" className="prof-custom-file-upload"> 
-            <p>Click to change Profile Picture</p>
-          </label>
-          <input className = "prof-upload-image" 
-            type="file" 
-            label="prof-image"
-            name="prof-image"
-            id="prof-file-upload"
-            accept=".jpeg, .png, .jpeg"
-            onChange={(e) => handleFileUpload(e)}
-          />
-          <button 
-            onClick={handlePicUpload}>Confirm Profile Pic</button>
-          </>
+          username === user.username && !hide && (
+          <div className="update-bio-pic">
+            <label htmlFor="prof-file-upload" className="prof-custom-file-upload"> 
+              <p>Click to change Profile Picture</p>
+            </label>
+            <input className = "prof-upload-image" 
+              type="file" 
+              label="prof-image"
+              name="prof-image"
+              id="prof-file-upload"
+              accept=".jpeg, .png, .jpeg"
+              onChange={(e) => handleFileUpload(e)}
+            />
+            <input
+              className="new-bio input-middle"
+              type="text" 
+              onChange={(e) => setNewAbout(e.target.value)}
+              value= {newAbout}
+              placeholder= 'Type new bio here...'
+            />
+            <button 
+              onClick={handlePicUpload}>Confirm changes
+            </button>
+          </div>
         )}
         <p>{about}</p>
+        <button onClick={handleHide}>Edit profile</button>
         <hr />
       </div>
       { user && user.username === username && <PostForm />}
